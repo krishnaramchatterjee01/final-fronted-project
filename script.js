692e4d332815f98debafad709444900f9327ee74
krishnaram Chatterjee <krishnaramchatterjee105@gmail.com>
2026-09-16 02:10:02 +0530

    page1 animation done

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function loadAnimation() {
     tl.from("#nav",{
         opacity:0
     })
-    tl.from(".hero h1",{
+    tl.from("#hero1 h1,#hero2 h1,#hero3 h1,#hero4 h1",{
         y:120,
         stagger: 0.2
     })
